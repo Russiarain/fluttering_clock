14a8e2ffef738b374e628e6a5f33fd1490569547
Frank Pang <pangjc2333@gmail.com>
2020-01-14 22:25:59 +0800

    initial code,most from digital clock.

---
+++ fluttering_clock/lib/fluttering_clock.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clock_helper/model.dart';
+import 'dart:async';
+
+class FlutteringClock extends StatefulWidget {
+  final ClockModel model;
+
+  const FlutteringClock(this.model);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FlutteringClockState();
+  }
+}
+
+class _FlutteringClockState extends State<FlutteringClock> {
+  DateTime _dateTime = DateTime.now();
+  Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.model.addListener(_updateModel);
+    _updateTime();
+    _updateModel();
+  }
+
+  @override
+  void didUpdateWidget(FlutteringClock oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.model != oldWidget.model) {
+      oldWidget.model.removeListener(_updateModel);
+      widget.model.addListener(_updateModel);
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    widget.model.removeListener(_updateModel);
+    widget.model.dispose();
+    super.dispose();
+  }
+
+  void _updateModel() {
+    setState(() {
+      // Cause the clock to rebuild when the model changes.
+    });
+  }
+
+  void _updateTime() {
+    setState(() {
+      _dateTime = DateTime.now();
+      // Update once per minute. If you want to update every second, use the
+      // following code.
+      _timer = Timer(
+        Duration(minutes: 1) -
+            Duration(seconds: _dateTime.second) -
+            Duration(milliseconds: _dateTime.millisecond),
+        _updateTime,
+      );
+      // Update once per second, but make sure to do it at the beginning of each
+      // new second, so that the clock is accurate.
+      // _timer = Timer(
+      //   Duration(seconds: 1) - Duration(milliseconds: _dateTime.millisecond),
+      //   _updateTime,
+      // );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
