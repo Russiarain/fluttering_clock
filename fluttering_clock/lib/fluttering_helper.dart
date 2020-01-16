fd113fdb2d04cc56dbe09d82617b0ec8c0d42784
Frank Pang <pangjc2333@gmail.com>
2020-01-16 22:46:18 +0800

    format code and remove useless comments

--- fluttering_clock/lib/fluttering_helper.dart
+++ fluttering_clock/lib/fluttering_helper.dart
@@ -10,7 +10,8 @@ class AnimatedBackgroud extends StatelessWidget {
       Track('color1').add(
           Duration(seconds: 3),
           ColorTween(
-              begin: Colors.purple.withAlpha(48), end: Colors.deepOrange.shade600)),
+              begin: Colors.purple.withAlpha(48),
+              end: Colors.deepOrange.shade600)),
       Track('color2').add(Duration(seconds: 3),
           ColorTween(begin: Color(0xffa83279), end: Colors.blue.shade600))
     ]);
@@ -71,9 +72,7 @@ class SineCurvePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    final cvPaint = Paint()
-      ..color =
-          Colors.white.withAlpha(48); //cvPaint is a 'Color' if only one dot
+    final cvPaint = Paint()..color = Colors.white.withAlpha(48);
     final path = Path();
 
     final areaHeight = size.height;
@@ -83,43 +82,27 @@ class SineCurvePainter extends CustomPainter {
     final double amplitude = 0.2;
 
     final y1 = sin(sineX);
-    final y2 = sin(sineX+pi/3);//sineX+60
-    final y3 = sin(sineX + 2*pi/3);
+    final y2 = sin(sineX + pi / 3);
+    final y3 = sin(sineX + 2 * pi / 3);
 
     final yStartPoint = areaHeight * (offset + amplitude * y1);
     final yControllPoint1 = areaHeight * (offset + amplitude * y2);
     final yControllPoint2 = areaHeight * (offset + amplitude * y3);
     final yEndPoint = areaHeight * (offset - amplitude * y1);
 
-    //test points start
-    final leftPoint = size.height * 0.8;
-    final midlePoint = size.height * 0.4;
-    final rightPoint = size.height * 0.6;
-    //test points end
-
     path.moveTo(0, yStartPoint);
-    path.cubicTo(areaWidth * 0.25, yControllPoint1, areaWidth * 0.5, yControllPoint2, areaWidth, yEndPoint);
-    //path.quadraticBezierTo(
-    //    areaWidth * 0.25, yControllPoint1, areaWidth * 0.5, yMiddlePoint);
-    //path.quadraticBezierTo(
-    //    areaWidth * 0.75, yControllPoint2, areaWidth, yBorderPoint);
-    //path.moveTo(0, yStartPoint);
-    //path.quadraticBezierTo(size.width * 0.5, yControllPoint, size.width, yEndPoint);
+    path.cubicTo(areaWidth * 0.25, yControllPoint1, areaWidth * 0.5,
+        yControllPoint2, areaWidth, yEndPoint);
     path.lineTo(areaWidth, areaHeight);
     path.lineTo(0, areaHeight);
 
     path.close();
 
-    /**
-     * path will close itself even without path.close().
-     * start point of path will be automatically connected to the end point.
-     */
     canvas.drawPath(path, cvPaint);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
     return true;
   }
 }
